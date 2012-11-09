f35529f4f81cf54157a472be46850e8fdf457d2b
Rubén López <ruben.lopez.angos@gmail.com>
2012-11-09 22:06:11 +0100

    Añadido eventListener de phonegap

--- js/basicos.js
+++ js/basicos.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function(){
+	document.addEventListener("deviceready", dispositivoListo, false);
 	jQuery('#boton1').click(function(){
 		alert("Has hecho click!");
 	});
 });
+
+function dispositivoListo(){
+	alert("Tu dispositivo está listo para usar la API de PhoneGap.");
+}
