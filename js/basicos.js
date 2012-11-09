3ca00fc1ce8f723befee937be3206abf7565def6
Rubén López <ruben.lopez.angos@gmail.com>
2012-11-09 23:17:33 +0100

    cambios en el listener

--- js/basicos.js
+++ js/basicos.js
@@ -1,10 +1,15 @@
-jQuery(document).ready(function(){
+/*jQuery(document).ready(function(){
 	document.addEventListener("deviceready", dispositivoListo, false);
 	jQuery('#boton1').click(function(){
 		alert("Has hecho click!");
 	});
 });
 
+function dispositivoListo(){
+	alert("Tu dispositivo está listo para usar la API de PhoneGap.");
+}*/
+
+document.addEventListener("deviceready", dispositivoListo, false);
 function dispositivoListo(){
 	alert("Tu dispositivo está listo para usar la API de PhoneGap.");
 }
