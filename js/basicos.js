f32a9058797e699ee1c27ec84daac2bf395825c4
Rubén López <ruben.lopez.angos@gmail.com>
2012-11-10 12:04:15 +0100

    Prueba de confirmaciones

--- js/basicos.js
+++ js/basicos.js
@@ -1,19 +1,13 @@
 jQuery(document).ready(function(){
 	document.addEventListener("deviceready", dispositivoListo, false);
-	jQuery('#boton1').click(function(){
-		alert("Has hecho click!");
-	});
 });
 
 function dispositivoListo(){
-	alert("Tu dispositivo está listo para usar la API de PhoneGap.");
-	navigator.geolocation.getCurrentPosition(lecturasGPS, errorGPS, {enableHighAccuracy:true});
+	jQuery('#boton').click(function(){
+		navigator.notification.confirm('Quieres aceptar la alerta?', alerta, 'Alerta', 'Si,No,Quizás');
+	});
 }
 
-function lecturaGPS(ubicacion){
-	alert("GPS activado y funcional!");
-}
+function alerta(boton){
 
-function errorGPS(error){
-	alert("GPS no disponible...");
 }
