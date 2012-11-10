748c3ff121fdfcc61531fb154ed072ba497874f3
Rubén López <ruben.lopez.angos@gmail.com>
2012-11-10 08:54:40 +0100

    pruebas otra vez

--- js/basicos.js
+++ js/basicos.js
@@ -1,4 +1,4 @@
-/*jQuery(document).ready(function(){
+jQuery(document).ready(function(){
 	document.addEventListener("deviceready", dispositivoListo, false);
 	jQuery('#boton1').click(function(){
 		alert("Has hecho click!");
@@ -7,4 +7,4 @@
 
 function dispositivoListo(){
 	alert("Tu dispositivo está listo para usar la API de PhoneGap.");
-}*/
+}
