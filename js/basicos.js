3a0d43bde8fc3a6e69bf49f60b73579a003176e5
Rubén López <ruben.lopez.angos@gmail.com>
2012-11-10 13:16:37 +0100

    agregar salir a un boton

--- js/basicos.js
+++ js/basicos.js
@@ -1,11 +1,15 @@
 jQuery(document).ready(function(){
 	setTimeout(function(){
 		document.addEventListener("deviceready", dispositivoListo, false);
-	}, 2000);
+	}, 3000);
 });
 
 function dispositivoListo(){
 	jQuery('#inicio-splash-screen').hide();
 	jQuery('#inicio-header').fadeIn(1000);
 	jQuery('#inicio-body').fadeIn(1000);
+	
+	jQuery('#boton').click(function(){
+		navigator.app.exitApp();
+	});
 }
