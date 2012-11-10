8af3a248d0670d4a5b21485aaa4c758198a1bd15
Rubén López <ruben.lopez.angos@gmail.com>
2012-11-10 13:30:54 +0100

    añadida prueba de menu

--- js/basicos.js
+++ js/basicos.js
@@ -6,8 +6,7 @@ jQuery(document).ready(function(){
 
 function dispositivoListo(){
 	jQuery('#inicio-splash-screen').hide();
-	jQuery('#inicio-header').fadeIn(1000);
-	jQuery('#inicio-body').fadeIn(1000);
+	jQuery('#inicio-contenedor').fadeIn(1000);
 	
 	jQuery('#boton').click(function(){
 		navigator.app.exitApp();
